8a59f397eeaf21defed58b7f8667ef5eba6c50e0
farah <farah@graaab.com>
2018-03-30 17:45:34 +0800

    Create search bar

--- app/src/main/java/com/graaab/intercomm/view/activities/MainActivity.java
+++ app/src/main/java/com/graaab/intercomm/view/activities/MainActivity.java
@@ -1,13 +1,22 @@
 package com.graaab.intercomm.view.activities;
 
+import android.content.Context;
 import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -16,8 +25,14 @@ import com.graaab.intercomm.R;
 import com.graaab.intercomm.view.activities.Tabs.TabContact;
 import com.graaab.intercomm.view.activities.Tabs.TabHistory;
 
-public class MainActivity extends AppCompatActivity
-        implements TabContact.OnFragmentInteractionListener, TabHistory.OnFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity{
+
+//    AppCompatActivity implements TabContact.OnFragmentInteractionListener, TabHistory.OnFragmentInteractionListener
+
+    private Toolbar toolbar;
+    private MenuItem mSearchAction;
+    private boolean isSearchOpened = false;
+    private EditText edtSeach;
 
     private int[] navIcons = { R.drawable.ic_contact, R.drawable.ic_history};
     private int[] navIconsActive = { R.drawable.ic_contact_active, R.drawable.ic_history_active};
@@ -28,46 +43,139 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+//        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+//        final ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
 
 //        for(int i= 0; i < navIcons.length ; i++){
 //            tabLayout.getTabAt(i).setIcon(navIcons[i]);
 //            tabLayout.getTabAt(i).setIcon(navIconsActive[i]);
 //        }
 
-        tabLayout.addTab(tabLayout.newTab().setText("Contact").setIcon(R.drawable.ic_contact_active));
-        tabLayout.addTab(tabLayout.newTab().setText("History").setIcon(R.drawable.ic_history));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+//        tabLayout.addTab(tabLayout.newTab().setText("Contact").setIcon(R.drawable.ic_contact_active));
+//        tabLayout.addTab(tabLayout.newTab().setText("History").setIcon(R.drawable.ic_history));
+//        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+//
+//
+//        final PagerAdapter adapter = new com.graaab.intercomm.adapter.PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+//        viewPager.setAdapter(adapter);
+//        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+//
+//        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+//            @Override
+//            public void onTabSelected(TabLayout.Tab tab) {
+//                viewPager.setCurrentItem(tab.getPosition());
+//
+//                tab.setIcon(navIconsActive[tab.getPosition()]);
+//            }
+//
+//            @Override
+//            public void onTabUnselected(TabLayout.Tab tab) {
+//
+//                tab.setIcon(navIcons[tab.getPosition()]);
+//            }
+//
+//            @Override
+//            public void onTabReselected(TabLayout.Tab tab) {
+//
+//            }
+//        });
+    }
 
+//    @Override
+//    public void onFragmentInteraction(Uri uri) {
+//
+//    }
 
-        final PagerAdapter adapter = new com.graaab.intercomm.adapter.PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-
-                tab.setIcon(navIconsActive[tab.getPosition()]);
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-                tab.setIcon(navIcons[tab.getPosition()]);
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+//        mSearchAction = menu.findItem(R.id.action_search);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
-    public void onFragmentInteraction(Uri uri) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_search:
+                handleMenuSearch();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void handleMenuSearch(){
+        ActionBar action = getSupportActionBar(); //get the actionbar
+
+        if(isSearchOpened){ //test if the search is open
+
+            action.setDisplayShowCustomEnabled(false); //disable a custom view inside the actionbar
+            action.setDisplayShowTitleEnabled(true); //show the title in the action bar
+
+            //hides the keyboard
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(edtSeach.getWindowToken(), 0);
+
+            //add the search icon in the action bar
+            mSearchAction.setIcon(getResources().getDrawable(R.drawable.ic_search_w));
+
+            isSearchOpened = false;
+        } else { //open the search entry
+
+            action.setDisplayShowCustomEnabled(true); //enable it to display a
+            // custom view in the action bar.
+            action.setCustomView(R.layout.search_bar);//add the custom view
+            action.setDisplayShowTitleEnabled(false); //hide the title
+
+            edtSeach = (EditText)action.getCustomView().findViewById(R.id.edtSearch_bar); //the text editor
+
+            //this is a listener to do a search when the user clicks on search button
+            edtSeach.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                        doSearch();
+                        return true;
+                    }
+                    return false;
+                }
+            });
+
+
+            edtSeach.requestFocus();
+
+            //open the keyboard focused in the edtSearch
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(edtSeach, InputMethodManager.SHOW_IMPLICIT);
+
+
+            //add the close icon
+            mSearchAction.setIcon(getResources().getDrawable(R.drawable.cancel));
+
+            isSearchOpened = true;
+        }
+    }
+
+    private void doSearch() {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(isSearchOpened) {
+            handleMenuSearch();
+            return;
+        }
+
+        super.onBackPressed();
     }
 }
